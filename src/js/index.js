9a6c64fad947dd12602e488eab6f1eabcc0febf1
Luana Palmeira <92049088+luanapalmeira@users.noreply.github.com>
2024-10-13 23:23:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+//OBJETIVO - quando clicar no pokedev da listagem temos que esconder o cartão do pokedev aberto.
+
+//PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem de pokedevs
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+//PASSO 2 - Identificar o evento de clique no elemento da listagem
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        //PASSO 3 - remover a classe aberto só do cartão que tá aberto
+        esconderCartaoPokedev();
+
+        //PASSO 4 - ao clicar em um pokedev da lista pegamos o id desse pokedev pra saber qual cartão abrir
+        const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+
+        //PASSO 5 - remover a classe ativo que marca o pokedev selecionado na listagem
+        desativarPokedevNaListagem();
+
+        //PASSO 6 - adicionar a classe ativo no pokedev selecionado na listagem
+        ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
